a5966ade718a14fd1c934f05a00bc2dab339649a
sparta40700 <valentinmercier.40@gmail.com>
2022-03-01 15:03:10 +0100

    startproject

---
+++ back/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+/***mongoose.connect(mongodb+srv://sparta40:<Dr@gon405001>@piquante.wttud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority) */
+module.exports = app;
